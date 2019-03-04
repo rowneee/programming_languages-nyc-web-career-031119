bea04fb8b23aa0fcb3074b3b2f6be5255c24961f
Roni Shabo <roni.shabo@gmail.com>
2019-03-04 19:50:39 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,9 +4,10 @@ def reformat_languages(languages)
     type_value.each do |language, type|
       if new_hash.has_key?(style)
         new_hash.delete(style)
-        style = "style"
-        new_hash.push(style)
-        style.to_sym
+        style_key = "style"
+        new_hash.push(style_key)
+        style_key = style_key.to_sym
+        [style_key] = 
         
       end
     end 
